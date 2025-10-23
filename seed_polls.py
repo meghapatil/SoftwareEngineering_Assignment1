a10da072a91805a9869f98ce77498acad1262702
Megha Patil <mp7464@nyu.edu>
2025-10-23 14:15:58 -0400

    adding polls

--- seed_polls.py
+++ seed_polls.py
@@ -1,14 +1,15 @@
 # seed_polls.py
-import os, django
-os.environ["SQLITE_PATH"] = "/var/app/data/db.sqlite3"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SEassignment1.settings")
-django.setup()
+import os
+import django
 from polls.models import Question, Choice
 from django.utils import timezone
 
+os.environ["SQLITE_PATH"] = "/var/app/data/db.sqlite3"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SEassignment1.settings")
+django.setup()
 q1, _ = Question.objects.get_or_create(
     question_text="Which framework do you like most?",
-    defaults={"pub_date": timezone.now()}
+    defaults={"pub_date": timezone.now()},
 )
 Choice.objects.get_or_create(question=q1, choice_text="Django", defaults={"votes": 0})
 Choice.objects.get_or_create(question=q1, choice_text="Flask", defaults={"votes": 0})
